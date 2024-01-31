ad680a87f6444942a8418d40d3f5c509580ca436
LA-Kings <158328045+LA-Kings@users.noreply.github.com>
2024-01-31 11:42:24 -0800

    Update seatingmap.js

--- seatingmap.js
+++ seatingmap.js
@@ -1,1 +1,49 @@
+$(document).ready(function(){
 
+    getMapHeight();
+
+    $( ".MapSectionInfo .section" ).hover(
+        function() {
+            var thisID = $( this ).attr('id');
+            var imgID = '#map_'+thisID;
+            $(imgID).siblings('.detailedImg').addClass('faded');
+        }, function() {
+            $('.detailedImg').removeClass('faded');
+        }
+    );
+
+
+    var resizeTimer;
+    $(window).on('resize', function(e) {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function() {
+            getMapHeight();
+            // makeFixed();
+        }, 250);
+    });
+
+
+    $('.triCard h2 a').click(function(){
+        var id = $(this).attr('id');
+
+        $('.MapSectionInfo ._fullSeason').css('display','none');
+        $('.MapSectionInfo ._halfSeason').css('display','none');
+        $('.MapSectionInfo ._flexPack').css('display','none');
+
+        $('.smallCopy ._fullSeason').css('display','none');
+        $('.smallCopy ._halfSeason').css('display','none');
+        $('.smallCopy ._flexPack').css('display','none');
+
+        $('.MapSectionInfo .'+ id ).css('display','block');
+        $('.smallCopy .'+ id ).css('display','block');
+
+
+    });
+
+});
+
+function getMapHeight(){
+    var imgHeight = $('#mapNumbers').height();
+    $('#MapCanvasWrapper').css('height',imgHeight+'px');
+
+}
